73aec669409e8e53e362b86088307a0fdfe35f0e
bcart <19235836@studentmail.ul.ie>
2022-04-20 00:13:54 +0100

    Checking new default branch works.

--- app/src/main/java/com/example/chirp/games/TicTacToeActivity.java
+++ app/src/main/java/com/example/chirp/games/TicTacToeActivity.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+//Tic Tac Toe class
 public class TicTacToeActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
